139a37a8b65bd3848a0eb5135f9716bb74786be6
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-02-23 17:47:44 -0600

    Add Home Nav Component with Animation

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,30 +1,35 @@
-import React, { useState } from 'react'
-import { AiOutlineHome, AiOutlineMenu } from 'react-icons/ai'
+import React, { useState } from 'react';
+import { CgMenuMotion } from 'react-icons/cg';
+import { BiSolidHomeHeart } from 'react-icons/bi';
 
 const Sidenav = () => {
-    const [nav, setNav] = useState(false)
+    const [nav, setNav] = useState(false);
     const handleNav = () => {
-        setNav(!nav)
-    }
+        setNav(!nav);
+    };
 
     return (
         <>
-            <AiOutlineMenu
+            <CgMenuMotion
                 onClick={handleNav}
                 className="absolute right-4 top-4 z-[99] md:hidden"
+                size={35}
             />
             {nav ? (
                 <div className="fixed z-20 flex h-screen w-full flex-col items-center justify-center bg-white/90">
-                    <a href="#main">
-                        <AiOutlineHome size={20} />
-                        <span className="pl-4">Home</span>
+                    <a
+                        href="#main"
+                        className="hover:bg-portafino-500 delay-50 m-2 flex w-[75%] cursor-pointer items-center justify-center rounded-full bg-gray-100 p-4 shadow-lg shadow-gray-400 transition duration-300 ease-in-out hover:hover:-translate-y-1 hover:scale-110 "
+                    >
+                        <BiSolidHomeHeart size={22} />
+                        <span className="pl-1">Home</span>
                     </a>
                 </div>
             ) : (
                 <div></div>
             )}
         </>
-    )
-}
+    );
+};
 
-export default Sidenav
+export default Sidenav;
