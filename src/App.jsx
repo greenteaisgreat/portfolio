9ef91db93fad1ac581fefd404174de7e6136d351
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-02-23 16:32:00 -0600

    Add Hamburger Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
-import './App.css'
 import Sidenav from './components/Sidenav'
 
 const App = () => {
-  return (
-    <>
-      <Sidenav />
-    </>
-  )
+    return (
+        <>
+            <Sidenav />
+        </>
+    )
 }
 
 export default App
--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import { AiOutlineMenu } from 'react-icons/ai'
+import React, { useState } from 'react'
+import { AiOutlineHome, AiOutlineMenu } from 'react-icons/ai'
 
 const Sidenav = () => {
-  return (
-    <div>
-        <AiOutlineMenu />
-    </div>
-  )
+    const [nav, setNav] = useState(false)
+    const handleNav = () => {
+        setNav(!nav)
+    }
+
+    return (
+        <>
+            <AiOutlineMenu
+                onClick={handleNav}
+                className="absolute right-4 top-4 z-[99] md:hidden"
+            />
+            {nav ? (
+                <div className="fixed z-20 flex h-screen w-full flex-col items-center justify-center bg-white/90">
+                    <a href="#main">
+                        <AiOutlineHome size={20} />
+                        <span className="pl-4">Home</span>
+                    </a>
+                </div>
+            ) : (
+                <div></div>
+            )}
+        </>
+    )
 }
 
 export default Sidenav
