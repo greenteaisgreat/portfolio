242a898a1b475b03fb3df84898530b0936f5094a
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-02-23 22:34:11 -0600

    Add Home Component with BG Color

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div
+            id="home"
+            className=" from-portafino-600 to-portage-500 to 90% via-portage-500 h-screen w-full bg-gradient-to-r from-70% via-70%"
+        ></div>
+    );
+};
+
+export default Home;
