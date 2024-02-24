7c577341c1560a33ddf02bb3baffcaab712a287c
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-02-24 17:25:38 -0600

    Reduce Opacity for Full Display & Add Hero Title

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,15 @@ const Home = () => {
     return (
         <div
             id="home"
-            className=" from-portafino-600 to-portage-500 to 90% via-portage-500 h-screen w-full bg-gradient-to-r from-70% via-70%"
-        ></div>
+            className="to 90% h-screen w-full bg-gradient-to-r from-portafino-600 from-70% via-portage-500 via-70% to-portage-500"
+        >
+            <div className="absolute left-0 top-0 h-screen w-full bg-white/10">
+                <div>
+                    <h1>Howdy! I'm Nathan</h1>
+                    <h2></h2>
+                </div>
+            </div>
+        </div>
     );
 };
 
