fb34ffe6f3924ed21512f2f54c2d267987bcda1e
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-02-25 16:17:28 -0600

    Add Logo for Small Viewports and Import in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Sidenav from './components/Sidenav';
 import Home from './components/Home';
 import Logo from './components/Logo';
+import SmLogo from './components/SmLogo';
 import Experience from './components/Experience';
 
 const App = () => {
     return (
         <>
+            <SmLogo />
             <Logo />
             <Sidenav />
             <Home />
