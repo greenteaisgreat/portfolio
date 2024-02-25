3c6588332438b6670769a800f160d338ca5247c8
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-02-24 21:34:22 -0600

    Add Logo Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Sidenav from './components/Sidenav';
 import Home from './components/Home';
+import Logo from './components/Logo';
 
 const App = () => {
     return (
         <>
+            <Logo />
             <Sidenav />
             <Home />
         </>
