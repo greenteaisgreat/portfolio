6bb88f4e17b5d64f7f55e30b81dbc9e957ba0dbe
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-02-25 16:18:07 -0600

    Adjust Size for Main Logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,9 +3,11 @@ import logo from '../assets/logo-1.svg';
 
 const Logo = () => {
     return (
-        <div className="top-15 absolute -right-10 hidden md:block">
-            <img src={logo} alt="logo" className="h-[18rem] w-[18rem]" />
-        </div>
+        <a href="#home">
+            <div className="top-15 absolute -right-10 hidden md:block">
+                <img src={logo} alt="logo" className="h-[18rem] w-[18rem]" />
+            </div>
+        </a>
     );
 };
 
