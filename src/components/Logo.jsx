fb8c5e94a0a0e24bacb027c86a9e46541bb66c90
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-02-24 21:45:02 -0600

    Resize Logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ import logo from '../assets/new-logo.svg';
 const Logo = () => {
     return (
         <div className="top-15 absolute right-0">
-            <img src={logo} alt="logo" className="h-[16rem] w-[16rem]" />
+            <img src={logo} alt="logo" className="h-[18rem] w-[18rem]" />
         </div>
     );
 };
