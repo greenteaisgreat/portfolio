42dc21c1a2e686234f4e19264c6d54d8c8ddf130
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-02-25 23:39:47 -0600

    Adjust Padding Top for Timeline

--- src/components/ExpItem.jsx
+++ src/components/ExpItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ExpItem = ({ year, title, company, details }) => {
     return (
-        <ol className="relative flex flex-col border-l border-stone-200 md:flex-row">
+        <ol className="relative flex flex-col border-l border-stone-200 pt-8 md:flex-row">
             <li className="mb-10 ml-4 ">
                 <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border-white bg-stone-200" />
                 <p className="flex flex-row flex-wrap items-center justify-start gap-4 text-xs md:text-sm">
