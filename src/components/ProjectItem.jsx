1f0a7c43ccd195ed51cb5bdb484d423a9f124697
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-02-25 22:16:21 -0600

    Add Description Parameter to all Experience Components

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 
-const ProjectItem = ({ img, title }) => {
+const ProjectItem = ({ img, title, language, description, link }) => {
     return (
-        <div className="rounded-xl bg-stone-200">
-            <img src={img} alt={title} className="rounded-t-xl" />
-            <div className="group relative flex h-auto w-full items-center justify-center rounded-b-xl from-portafino-500 to-portage-500  hover:bg-gradient-to-r">
-                <h3 className="rounded-b-xl">{title}</h3>
+        <div className="group relative flex h-auto w-full items-center justify-center rounded-xl from-portafino-100  to-portage-300 hover:bg-gradient-to-r">
+            <img
+                src={img}
+                alt={title}
+                className="rounded-xl group-hover:opacity-30"
+            />
+            <div className="absolute left-[50%] top-[50%] hidden translate-x-[-50%] translate-y-[-50%] group-hover:block">
+                <h3 className="text-center text-5xl font-bold tracking-wider text-gray-800">
+                    {title}
+                </h3>
+                <p className="text-md pt-5 text-center font-bold text-black">
+                    {description}
+                </p>
+                <p className="text-md pb-12 pt-4 text-center font-bold text-black">
+                    {language}
+                </p>
+                <a href={link}>
+                    <p className="cursor-pointer rounded-lg bg-portage-300 p-3 text-center text-lg font-bold text-gray-700 hover:bg-portafino-100 active:bg-portage-600">
+                        Check It Out!
+                    </p>
+                </a>
             </div>
         </div>
     );
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,27 +9,50 @@ const Projects = () => {
     return (
         <div className="h-full w-full bg-portage-400 ">
             <div
-                id="projects"
+                id="opensource"
                 className="m-auto max-w-[1040px] p-4 py-12 md:pl-20"
             >
-                <h1 className="text-center text-4xl font-bold text-[#001b5e]">
+                <h1 className="text-center text-4xl font-bold text-black">
                     Open-Source Contributions
                 </h1>
-                <p className="py-8 text-center">
+                <p className="py-8 pb-12 text-center text-xl font-medium">
                     I'm of the firm belief that the web we've all come to know
                     and love (and heavily rely upon) has its backbone built upon
                     open-source technologies. As a proponent of this ideology, I
-                    strive to collaborate on open-source projects, be it from
-                    friends I know in person or friends from across the globe.
-                    Here's a sample of some of my works:
+                    strive to collaborate on open-source projects as often as I
+                    can, be it from friends I know in person or friends from
+                    across the globe. <br />
+                    <br />
+                    Here's a sample of some of my recent works:
                 </p>
-                <div className="grid gap-12 sm:grid-cols-2">
-                    <ProjectItem img={prevueImg} title="PreVue" />
-                    <ProjectItem img={pomonotoImg} title="Pomonoto!" />
-                    <ProjectItem img={weatherImg} title="Weather.io" />
+                <div className="-sm:grid-cols-2 grid gap-12 md:grid-cols-2">
+                    <ProjectItem
+                        img={prevueImg}
+                        title="PreVue3"
+                        description="All-in-One Prototyping Tool for Vue Engineers"
+                        language="Vue.js / Express.js / MongoDB"
+                        link="https://github.com/oslabs-beta/PreVue3.0"
+                    />
+                    <ProjectItem
+                        img={pomonotoImg}
+                        title="Pomonoto!"
+                        description="Combination Pomodoro Timer & Note Taking App"
+                        language="React.js / Express.js / MongoDB"
+                        link="https://github.com/greenteaisgreat/Pomonoto"
+                    />
+                    <ProjectItem
+                        img={weatherImg}
+                        title="Weather.io"
+                        description="Check the Weather Instantly! Powered by OpenWeatherMap API"
+                        language="React.js / Express.js / PostgreSQL"
+                        link="https://github.com/Weather-io/Weather.io"
+                    />
                     <ProjectItem
                         img={stopSmokingImg}
                         title="Stop Smoking Tracker"
+                        description="Increase Your Chances of Quitting a Bad Habit via Cognizance"
+                        language="Vanilla JavaScript"
+                        link="https://github.com/greenteaisgreat/stop-smoking-tracker"
                     />
                 </div>
             </div>
