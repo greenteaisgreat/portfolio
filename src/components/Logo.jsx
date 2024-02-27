05a9b20660e454d85c5f94a9a9602284ac8d5004
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-02-26 18:01:20 -0600

    Adjust Logo Absolute Position, Causing Overflow

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ import logo from '../assets/logo-1.svg';
 const Logo = () => {
     return (
         <a href="#home">
-            <div className="top-15 absolute -right-10 hidden md:block">
+            <div className="top-15 absolute -right-0 hidden md:block">
                 <img src={logo} alt="logo" className="h-[18rem] w-[18rem]" />
             </div>
         </a>
