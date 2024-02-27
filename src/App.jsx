aeb8fa4b25a86aabf951bdc1c6bc58f4e38b7816
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-02-26 20:39:47 -0600

    Import Footer Component into App

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import SmLogo from './components/SmLogo';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 const App = () => {
     return (
@@ -16,6 +17,7 @@ const App = () => {
             <Experience />
             <Projects />
             <Contact />
+            <Footer />
         </>
     );
 };
