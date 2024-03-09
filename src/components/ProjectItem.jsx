15f45b654f602f09db4a1e4b50aeebb309fad5e5
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-03-08 23:17:30 -0600

    Change ProjectItem Styling to Full Height

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ProjectItem = ({ img, title, language, description, link }) => {
     return (
-        <div className="delay-40 group relative flex h-auto w-full items-center justify-center rounded-xl from-portafino-100 to-portage-300 transition duration-300 hover:-translate-y-1 hover:bg-gradient-to-r">
+        <div className="delay-40 group relative flex h-full w-full items-center justify-center rounded-xl from-portafino-100 to-portage-300 transition duration-300 hover:-translate-y-1 hover:bg-gradient-to-r">
             <img
                 src={img}
                 alt={title}
@@ -12,13 +12,13 @@ const ProjectItem = ({ img, title, language, description, link }) => {
                 <h3 className="text-center text-5xl font-bold tracking-wider text-gray-800 drop-shadow-2xl">
                     {title}
                 </h3>
-                <p className="pt-4 text-center text-xl font-bold text-black drop-shadow-2xl">
+                <p className="pt-4 text-center text-2xl font-bold text-black drop-shadow-2xl">
                     {description}
                 </p>
-                <p className="pb-6 pt-4 text-center text-sm font-bold text-black drop-shadow-2xl">
+                <p className="pb-6 pt-4 text-center text-lg font-bold text-black drop-shadow-2xl">
                     {language}
                 </p>
-                <a href={link}>
+                <a href={link} target="_blank">
                     <p className="cursor-pointer rounded-lg bg-portage-300 p-3 text-center text-lg font-bold text-gray-700 transition duration-300 hover:-translate-y-1 hover:bg-portafino-100 active:bg-portage-600">
                         Find Out More
                     </p>
